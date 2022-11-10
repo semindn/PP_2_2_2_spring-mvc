caf4f94bb48b16ddc692e77f7f0550e02b8615dd
semindn <semindn@yandex.ru>
2022-11-10 11:53:39 +0300

    Задача 2.2.2 (решение)

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +21,29 @@ public class HelloController {
 		messages.add("5.2.0 version by sep'19 ");
 		model.addAttribute("messages", messages);
 		return "index";
+	}
+
+	@GetMapping(value = "/cars")
+	public String printCars(
+			@RequestParam(value = "count", required = false) Integer countCars,
+			ModelMap model) {
+		Car car1 = new Car("model1", "color1", 101);
+		Car car2 = new Car("model2", "color2", 102);
+		Car car3 = new Car("model3", "color3", 103);
+		Car car4 = new Car("model4", "color4", 104);
+		Car car5 = new Car("model5", "color5", 105);
+		List<Car> list = new ArrayList<>();
+		list.add(car1);
+		list.add(car2);
+		list.add(car3);
+		list.add(car4);
+		list.add(car5);
+		if (countCars == null || countCars < 0 || countCars > list.size()) {
+			countCars = list.size();
+		}
+		model.addAttribute("cars", list.stream().limit(countCars).toList());
+		model.addAttribute("countCars", countCars);
+		return "cars.html";
 	}
 	
 }
---
+++ src/main/java/web/model/Car.java
@@ -0,0 +1,46 @@
+package web.model;
+
+public class Car {
+    private String model;
+    private String color;
+    private int horsePower;
+
+    public Car(String model, String color, int horsePower) {
+        this.model = model;
+        this.color = color;
+        this.horsePower = horsePower;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    @Override
+    public String toString() {
+        return "Car: " +
+                "model '" + model + '\'' +
+                ", color '" + color + '\'' +
+                ", horsePower " + horsePower +
+                '.';
+    }
+}
